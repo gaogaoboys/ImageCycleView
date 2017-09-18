1219ad7a598b369e8f40ab82d8f635195ebf6953
gaogaoboys <gaogaoboys@163.com>
2017-09-18 16:20:32 +0800

    增加Hello类

---
+++ src/com/minking/imagecycleview/Hello.java
@@ -0,0 +1,12 @@
+package com.minking.imagecycleview;
+
+/**
+ * Created by Host-0164 on 2017/9/18.
+ */
+
+public class Hello {
+    public String hell(){
+        System.out.println("hello");
+        return "hello";
+    }
+}
